5ca70c31f18e0870154a62044485cc037f9e86ee
Kevin Johnson <kljohnson@xactware.com>
2017-04-12 11:01:42 -0600

    Adds the initial project structure
    issue #2
    
    Why:
    
    * I need the project to exist before I can implement the init verb
    
    This change addresses the need by:
    
    * Adding the initial csproj and sln files

---
+++ src/GitComponentVersion.UnitTests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GitComponentVersion.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.IsTrue(true, "This should be working");
+        }
+    }
+}
